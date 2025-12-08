37c0565b64e49a7d96e088d72ed4281f90144e63
saruj <14skd12@gmail.com>
2025-12-08 10:33:23 +0530

    extra

--- app/src/main/java/com/skd/audioplayer/MainActivity.kt
+++ app/src/main/java/com/skd/audioplayer/MainActivity.kt
@@ -632,7 +632,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     companion object {
         private const val PERMISSION_REQUEST_CODE = 123
         private const val CHANNEL_ID = "MusicPlayerChannel"
