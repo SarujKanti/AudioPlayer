a0004d7f33bf833ae631e504eb7b1bca3038e430
saruj <14skd12@gmail.com>
2026-01-20 00:22:52 +0530

    in mainActivity changed permission

--- app/src/main/java/com/skd/audioplayer/MainActivity.kt
+++ app/src/main/java/com/skd/audioplayer/MainActivity.kt
@@ -633,7 +633,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     companion object {
         private const val PERMISSION_REQUEST_CODE = 123
         private const val CHANNEL_ID = "MusicPlayerChannel"
