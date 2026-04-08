e38a7c596f20a265483aaad3861e1334b73f7f9d
saruj <14skd12@gmail.com>
2026-04-08 11:19:47 +0530

    gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.skd.audioplayer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 1
+        versionName = "1.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
